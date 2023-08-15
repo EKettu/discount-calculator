855f6812e9de8227c694c7eb9bdd75d5ab4a3618
EKettu <3268745+EKettu@users.noreply.github.com>
2023-08-15 13:54:57 +0300

    refactoring app test

--- frontend/tests/App.test.jsx
+++ frontend/tests/App.test.jsx
@@ -1,6 +1,6 @@
 import { act, render, screen } from '@testing-library/react';
 import App from "../src/App";
-import {describe, it, expect, vi} from 'vitest';
+import {describe, it, expect, vi, beforeEach} from 'vitest';
 import { BrowserRouter as Router } from 'react-router-dom';
 import axios from 'axios';
 import { customers } from './testData/testCustomers';
@@ -9,9 +9,9 @@ import { discounts } from './testData/testDiscounts';
 
 vi.mock('axios')
 
-describe('App basic texts', () => {
+describe('App basic elements', () => {
 
-    it('renders app with Home', async () => {
+    beforeEach(async() => {
         await act(() => { axios.get.mockResolvedValue({
             products: products,
             customers: customers,
@@ -22,7 +22,17 @@ describe('App basic texts', () => {
                 <App />
             </Router>
         )})
+      });
+
+    it('renders app with a proper headline', async () => {
         const headline = screen.getByText(/Discount Calculator/i);
         expect(headline).toBeInTheDocument();
+    });
+
+    it('renders app with proper links', async () => {
+        expect(screen.getByRole('link', { name: 'Home' })).toHaveAttribute('href', `/`);
+        expect(screen.getByRole('link', { name: 'Products' })).toHaveAttribute('href', `/products`);
+        expect(screen.getByRole('link', { name: 'Customers' })).toHaveAttribute('href', `/customers`);
+        expect(screen.getByRole('link', { name: 'Discounts' })).toHaveAttribute('href', `/discounts`);
     });
 });
